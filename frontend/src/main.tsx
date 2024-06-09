59ccf6b26bf51543b207623254a2f7105c5b28ae
maricus <mo@myworkpay.com>
2024-06-09 07:21:47 +0300

    initialise project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
+
+loadDevMessages();
+loadErrorMessages();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
